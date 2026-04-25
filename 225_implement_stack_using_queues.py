73d70d8a7d4fbeca98542d89c124743525881150
Akhil Senthil <akhilsenthil696@gmail.com>
2026-04-25 23:35:00 +0530

    Add solution for LeetCode 225

---
+++ 225_implement_stack_using_queues.py
@@ -0,0 +1,27 @@
+"""
+LeetCode 225. Implement Stack using Queues
+
+Problem summary:
+- Implement a LIFO stack using queue operations.
+"""
+
+from collections import deque
+
+
+class MyStack:
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x):
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self):
+        return self.q.popleft()
+
+    def top(self):
+        return self.q[0]
+
+    def empty(self):
+        return len(self.q) == 0
